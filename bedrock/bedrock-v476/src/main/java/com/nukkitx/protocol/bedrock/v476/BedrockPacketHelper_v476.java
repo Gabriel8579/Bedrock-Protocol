5d11596b8e46a3d97fd5a8f1a699edd6a65c22ad
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2021-11-28 13:12:58 +0100

    MCBE 1.18.10 | Update Protocol

---
+++ bedrock/bedrock-v476/src/main/java/com/nukkitx/protocol/bedrock/v476/BedrockPacketHelper_v476.java
@@ -0,0 +1,7 @@
+package com.nukkitx.protocol.bedrock.v476;
+
+import com.nukkitx.protocol.bedrock.v471.BedrockPacketHelper_v471;
+
+public class BedrockPacketHelper_v476 extends BedrockPacketHelper_v471 {
+    public static final BedrockPacketHelper_v476 INSTANCE = new BedrockPacketHelper_v476();
+}
